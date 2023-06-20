ae8f61e2aa8a55060a4def0ddb8d176a315bef63
Jazthedev <joelpepa2@gmail.com>
2023-06-20 16:27:01 +0200

    add query subscription status

--- src/pages/subscriptions/index.jsx
+++ src/pages/subscriptions/index.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import { Link } from "react-router-dom";
 import { NFT_PAGE } from "../../utils/pages";
 import { AiOutlineCheckSquare } from 'react-icons/ai'
+import axios from "axios";
 const Subscription = () => {
-    const [isSuscribed,setSubscription] = useState();
+    const [isSuscribed,setSubscription] = useState(false);
     const [daysLeft,setDaysLeft] = useState(0);
+    const [loading,setLoading] = useState(true);
+    useEffect(()=>{
+        const getData = async ()=> {
+            const {data} = await axios.get(`http://localhost:3000/api/userSuscribed?address=0x00000000000000000000000000000000003d3403`)
+            setSubscription(data?.data?.isUsersuscribed);
+            console.log(data)
+            setLoading(false);
+        }
+        
+        getData();
+
+    },[])
+    
     return (
         <div className="w-full center flex-col ">
             <div className="flex-col center py-28 gap-16 w-full">
@@ -70,7 +84,8 @@ const Subscription = () => {
                     {isSuscribed ? <div className="p-6 bg-white rounded-lg flex flex-col justify-between items-center h-full shadow w-[30%]">
                         <p className="uppercase text-black oxanium tracking-widest font-bold mb-4 center">Subscription Expiry Time</p>
 
-                        <h3 className="lato text-[3rem] font-bold leading-[50px] text-center text-primary">{daysLeft} Days left</h3>
+                        {/* <h3 className="lato text-[3rem] font-bold leading-[50px] text-center text-primary">{daysLeft} Days left</h3> */}
+                        <h3 className="lato text-[3rem] font-bold leading-[50px] text-center text-primary">Suscription Bought, Enjoy!</h3>
 
                         <div className="flex flex-col gap-2">
                             <div className="items-start flex gap-3">
@@ -82,7 +97,7 @@ const Subscription = () => {
 
 
                         <button className="py-3 oxanium tracking-widest w-full bg-primary text-white rounded-md text-sm  duration-300">
-                            Suscription Bought
+                            Play
                         </button>
 
                     </div> : <div className="p-6 bg-white rounded-lg flex flex-col justify-between items-center h-full shadow w-[30%]">
