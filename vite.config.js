4534fb0231132a2cd8729c92d760d96376204ab6
Olumide Micheal <olumidemicheal7@gmail.com>
2023-06-23 18:38:06 +0100

    fixed hosting using commonjs

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import commonjs from 'vite-plugin-commonjs'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills(),
+    commonjs(),
+  ],
 });
